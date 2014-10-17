290cf662c80ce37d71e18c6510f5625b1cf5a18b
baole-lunex <baole@lunextelecom.com>
2014-10-17 16:40:57 +0700

    - support command line

--- event-processor-input/src/main/java/com/lunex/eventprocessor/input/InputProcessorLaunch.java
+++ event-processor-input/src/main/java/com/lunex/eventprocessor/input/InputProcessorLaunch.java
@@ -1,15 +1,21 @@
 package com.lunex.eventprocessor.input;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
+import java.util.Arrays;
 
 import kafka.serializer.StringEncoder;
 
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.commons.cli.UnrecognizedOptionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Strings;
 import com.lunex.eventprocessor.input.kafka.ASCIIPartitioner;
 import com.lunex.eventprocessor.input.kafka.KafkaProducer;
 import com.lunex.eventprocessor.input.netty.NettyHttpSnoopServer;
@@ -30,16 +36,42 @@ public class InputProcessorLaunch {
   public static SeqTimerTask seqTimerTask;
   public static KafkaProducer kafkaProducer;
 
+  /** The admin */
+  private static final String OPTION_APP = "a";
+  private static final String OPTION_HELP = "h";
+
   public static void main(String[] args) {
     logger.info("Hello event processor!");
     try {
-      // load log properties
-      Properties props = new Properties();
-      props.load(new FileInputStream("conf/log4j.properties"));
-      PropertyConfigurator.configure(props);
+
+      final Options options = new Options();
+      options.addOption(null, OPTION_APP, true, "app.properties");
+      options.addOption(null, OPTION_HELP, false, "Display command line help.");
+      final CommandLineParser parser = new PosixParser();
+      final CommandLine cmd;
+      try {
+        cmd = parser.parse(options, args);
+        if (cmd.getArgs().length > 0) {
+          throw new UnrecognizedOptionException("Extra arguments were provided in "
+              + Arrays.asList(args));
+        }
+      } catch (final ParseException e) {
+        printHelp(options, "Could not parse command line: " + Arrays.asList(args));
+        return;
+      }
+      if (cmd.hasOption(OPTION_HELP) || !cmd.hasOption(OPTION_APP)) {
+        printHelp(options, null);
+        return;
+      }
 
       // Load config
-      Configuration.getPropertiesValues("conf/app.properties");
+      String appConfig = cmd.getOptionValue(OPTION_APP);
+      Configuration.getPropertiesValues(appConfig);
+
+      // load log properties
+      // Properties props = new Properties();
+      // props.load(new FileInputStream("conf/log4j.properties"));
+      // PropertyConfigurator.configure(props);
 
       // start netty server
       InputProcessorLaunch.startHttpServer();
@@ -58,6 +90,14 @@ public class InputProcessorLaunch {
     } catch (Exception e) {
       logger.error(e.getMessage());
     }
+  }
+
+  private static void printHelp(final Options options, final String errorMessage) {
+    if (!Strings.isNullOrEmpty(errorMessage)) {
+      System.err.println(errorMessage);
+    }
+    final HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp("input-processor", options);
   }
 
   /**
