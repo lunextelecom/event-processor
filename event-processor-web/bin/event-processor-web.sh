96ff5c7ef034d96378a2638afa05413ea0fdd25c
baole-lunex <baole@lunextelecom.com>
2014-10-27 17:48:15 +0700

    - update

--- event-processor-web/bin/event-processor-web.sh
+++ event-processor-web/bin/event-processor-web.sh
@@ -6,14 +6,6 @@ key="$1"
 shift
 
 case $key in
-	-a)
-	APP="$1"
-	shift
-	;;
-	-p)
-	PROXY="$1"
-	shift
-	;;
 	-c)
 	CONF="$1"
 	shift
@@ -32,32 +24,12 @@ case $key in
 esac
 done
 
-if [ -z "$APP" ]; then
-	APP=$DEPLOY_DIR/conf/queue.properties
-fi
-if [ -z "$PROXY" ]; then
-	PROXY=$DEPLOY_DIR/conf/proxy.properties
-fi
 if [ -z "$CONF" ]; then
 	CONF=$DEPLOY_DIR/conf/configuration.yaml
 fi
 if [ -z "$LOG" ]; then
 	LOG=$DEPLOY_DIR/conf/log4j.properties
 fi
-
-f=$APP
-if [ -d "$f" ]; then f=$f/.; fi
-absolute=$(cd "$(dirname -- "$f")"; printf %s. "$PWD")
-absolute=${absolute%?}
-absolute=$absolute/${f##*/}
-APP=$absolute
-
-f=$PROXY
-if [ -d "$f" ]; then f=$f/.; fi
-absolute=$(cd "$(dirname -- "$f")"; printf %s. "$PWD")
-absolute=${absolute%?}
-absolute=$absolute/${f##*/}
-PROXY=$absolute
 
 f=$CONF
 if [ -d "$f" ]; then f=$f/.; fi
@@ -73,4 +45,4 @@ absolute=${absolute%?}
 absolute=$absolute/${f##*/}
 LOG=$absolute
 
-exec java -jar -Xms2500m -Xmx2500m -Dlog4j.configuration=file:$LOG $DEPLOY_DIR/lib/event-processor-web-1.0-SNAPSHOT.jar -a $APP -p $PROXY -c $CONF
+exec java -jar -Xms2500m -Xmx2500m -Dlog4j.configuration=file:$LOG $DEPLOY_DIR/lib/event-processor-web-1.0-SNAPSHOT.jar -c $CONF
