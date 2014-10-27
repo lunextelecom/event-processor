db567d51789b4252408f6efc6689840e3efb692d
duynguyen-lunex <duynguyen@lunextelecom.com>
2014-10-27 10:19:33 +0700

    update

---
+++ event-processor-core/src/main/java/com/lunex/eventprocessor/core/ResultComputation.java
@@ -0,0 +1,76 @@
+package com.lunex.eventprocessor.core;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.lunex.eventprocessor.core.utils.JsonHelper;
+import com.lunex.eventprocessor.core.utils.StringUtils;
+
+public class ResultComputation implements Serializable {
+ 
+  private static final long serialVersionUID = 8813738595877560011L;
+
+  private long time = System.currentTimeMillis();
+  private String eventName;
+  private String ruleName;
+  private Map<String, Object> result = new HashMap<String, Object>();
+  private String payLoadStr;
+  private String hashKey;
+  
+  public ResultComputation(String eventName, String ruleName, long time, String hashKey, String payLoad) {
+    try {
+      JSONObject jsonObject = new JSONObject(payLoad);
+      this.result = JsonHelper.toMap(jsonObject);
+      this.eventName = eventName;
+      this.ruleName = ruleName;
+      this.time = time;
+      this.hashKey = hashKey;
+    } catch (Exception ex) {
+      this.eventName = null;
+      this.ruleName = null;
+      this.result = null;
+      this.time = -1;
+    }
+  }
+  
+  public long getTime() {
+    return time;
+  }
+  public void setTime(long time) {
+    this.time = time;
+  }
+  public String getEventName() {
+    return eventName;
+  }
+  public void setEventName(String eventName) {
+    this.eventName = eventName;
+  }
+  public String getRuleName() {
+    return ruleName;
+  }
+  public void setRuleName(String ruleName) {
+    this.ruleName = ruleName;
+  }
+  public Map<String, Object> getResult() {
+    return result;
+  }
+  public void setResult(Map<String, Object> result) {
+    this.result = result;
+  }
+  public String getPayLoadStr() {
+    return payLoadStr;
+  }
+  public void setPayLoadStr(String payLoadStr) {
+    this.payLoadStr = payLoadStr;
+  }
+  public String getHashKey() {
+    return hashKey;
+  }
+  public void setHashKey(String hashKey) {
+    this.hashKey = hashKey;
+  }
+
+}
--- event-processor-handler/src/main/java/com/lunex/eventprocessor/handler/output/CheckConditionDayOfWeek.java
+++ event-processor-handler/src/main/java/com/lunex/eventprocessor/handler/output/CheckConditionDayOfWeek.java
@@ -1,6 +1,8 @@
 package com.lunex.eventprocessor.handler.output;
 
+import java.util.Calendar;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import javax.script.ScriptEngine;
@@ -11,9 +13,12 @@ import org.slf4j.LoggerFactory;
 
 import com.lunex.eventprocessor.core.EventQuery;
 import com.lunex.eventprocessor.core.EventResult;
+import com.lunex.eventprocessor.core.ResultComputation;
+import com.lunex.eventprocessor.core.dataaccess.CassandraRepository;
 import com.lunex.eventprocessor.core.utils.Constants;
 import com.lunex.eventprocessor.core.utils.JsonHelper;
 import com.lunex.eventprocessor.core.utils.StringUtils;
+import com.lunex.eventprocessor.handler.utils.Configurations;
 
 public class CheckConditionDayOfWeek implements CheckConditionHandler {
 
@@ -22,7 +27,7 @@ public class CheckConditionDayOfWeek implements CheckConditionHandler {
   public EventResult checkCondition(Map<String, Object> properties, EventQuery eventQuery,
       String hashKey) {
     String eventQueryCondition = eventQuery.getConditions();
-    if (eventQueryCondition != null && !Constants.EMPTY_STRING.equals(eventQueryCondition)) {
+    if (eventQueryCondition != null && !Constants.EMPTY_STRING.equals(eventQueryCondition) && eventQuery.getWeight() != null) {
       if (!properties.keySet().isEmpty()) {
         Iterator<String> keys = properties.keySet().iterator();
         while (keys.hasNext()) {
@@ -31,11 +36,44 @@ public class CheckConditionDayOfWeek implements CheckConditionHandler {
           eventQueryCondition =
               eventQueryCondition.replace(newkey, String.valueOf(properties.get(key)));
         }
+      }
+      long dateMiliseconds = 24*60*60*1000;
+      double avgAmount = 0.0;
+      double totalAmount = 0.0;
+      int numWeek = 0;
+      for(int i = 1; i <= 4; i++){
+        long passTime = dateMiliseconds*i*eventQuery.getWeight();
+        long startTime = getBeginTime(passTime);
+        long endTime = getEndTime(passTime);
+        try {
+          List<ResultComputation> lst = CassandraRepository.getInstance(Configurations.cassandraHost,
+              Configurations.cassandraKeyspace).getResultComputation(eventQuery.getEventName(), eventQuery.getRuleName(), startTime, endTime, 1);
+          if(lst != null && lst.size()>0){
+            ResultComputation resultComputation = lst.get(0);
+            if(resultComputation.getResult().get("amount") != null && Double.parseDouble(resultComputation.getResult().get("amount").toString())>0){
+              totalAmount = Double.parseDouble(resultComputation.getResult().get("amount").toString());
+              numWeek++;
+            }
+          }
+        } catch (Exception e) {
+        }
+      }
+      if(numWeek>0){
+        avgAmount = totalAmount/numWeek;
+      }
+      if(avgAmount==0.0){
+        EventResult eventResult =
+            new EventResult(eventQuery.getEventName(), String.valueOf(hashKey),
+                "{\"result\": false, \"exception\": \"" + "avgAmount = 0" + "\", \"reult\": \""
+                    + eventQuery.getRuleName() + "\"}", null);
+        return eventResult;
+      }else{
         //
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine engine = mgr.getEngineByName("JavaScript");
         try {
           // check condition for EventQuery
+          eventQueryCondition = eventQueryCondition.replace("avgval", String.valueOf(avgAmount));
           boolean checked = (Boolean) engine.eval(eventQueryCondition);
           logger.info("Result:" + checked + " - " + properties.toString());
           properties = StringUtils.revertHashMapField(properties);
@@ -56,8 +94,40 @@ public class CheckConditionDayOfWeek implements CheckConditionHandler {
           return eventResult;
         }
       }
+      
     }
     return null;
   }
-
+  public static void main(String[] args) {
+    long currentDateTime = System.currentTimeMillis();
+    System.out.println(currentDateTime);
+  
+    //Converting milliseconds to Date using Calendar
+    Calendar cal = Calendar.getInstance();
+    cal.setTimeInMillis(currentDateTime);
+    cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE), 0, 0, 0);
+    System.out.println(cal.getTime().getTime());
+    
+    System.out.println(currentDateTime);
+    long t = 1414141281521l;
+    System.out.println("1414141281521");
+//    1414141281521
+    
+  }
+  
+  private long getBeginTime(long time) {
+    //Converting milliseconds to Date using Calendar
+    Calendar cal = Calendar.getInstance();
+    cal.setTimeInMillis(time);
+    cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE), 0, 0, 0);
+    return cal.getTime().getTime();
+  }
+  
+  private long getEndTime(long time) {
+    //Converting milliseconds to Date using Calendar
+    Calendar cal = Calendar.getInstance();
+    cal.setTimeInMillis(time);
+    cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE), 23, 59, 59);
+    return cal.getTime().getTime();
+  }
 }
