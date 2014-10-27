095bd66c80d098ca142be711a39c2b6cce7bd585
baole-lunex <baole@lunextelecom.com>
2014-10-27 18:00:44 +0700

    - update

--- event-processor-web/bin/event-processor-web.sh
+++ event-processor-web/bin/event-processor-web.sh
@@ -1,34 +1,34 @@
-#!/bin/bash
 DEPLOY_DIR=$(dirname $0)/..
+echo DEPLOY_DIR
 while [[ $# > 1 ]]
 do
 key="$1"
 shift
 
 case $key in
-	-c)
-	CONF="$1"
-	shift
-	;;
-	-l)
-	LOG="$1"
-	shift
-	;;
-	--default)
-	DEFAULT=YES
-	shift
-	;;
-	*)
-	# unknown option
-	;;
+        -c)
+        CONF="$1"
+        shift
+        ;;
+        -l)
+        LOG="$1"
+        shift
+        ;;
+        --default)
+        DEFAULT=YES
+        shift
+        ;;
+        *)
+        # unknown option
+        ;;
 esac
 done
 
 if [ -z "$CONF" ]; then
-	CONF=$DEPLOY_DIR/conf/configuration.yaml
+        CONF=$DEPLOY_DIR/conf/configuration.yaml
 fi
 if [ -z "$LOG" ]; then
-	LOG=$DEPLOY_DIR/conf/log4j.properties
+        LOG=$DEPLOY_DIR/conf/log4j.properties
 fi
 
 f=$CONF
@@ -46,3 +46,4 @@ absolute=$absolute/${f##*/}
 LOG=$absolute
 
 exec java -jar -Xms2500m -Xmx2500m -Dlog4j.configuration=file:$LOG $DEPLOY_DIR/lib/event-processor-web-1.0-SNAPSHOT.jar -c $CONF
+
